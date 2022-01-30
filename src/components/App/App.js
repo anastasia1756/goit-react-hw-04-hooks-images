9d446407f458fd7bcef54508ec350e68607dca35
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-01-30 19:55:29 +0200

    changing class on hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -27,7 +27,7 @@ export const App = () => {
         newImages.length === 0 && toast.error("No such picture");
         setLoading(true);
         setLoading(false);
-        setImages(newImages);
+        setImages((state) => [...state, ...newImages]);
       } catch (error) {
         setError(error.message);
       }
@@ -39,12 +39,12 @@ export const App = () => {
     setShowModal((state) => !state);
     setImageForModal(image);
   };
-  const loadMoreBtn = () => {
+  const loadMoreBtn = async () => {
     setPage((state) => state + 1);
-    setLoading(true);
   };
-  const handleFormSubmit = (image) => {
+  const handleFormSubmit = async (image) => {
     setPage(1);
+    setImages([]);
     setValue(image);
   };
   const lengthOfImages = images.length;
@@ -68,7 +68,7 @@ export const App = () => {
         />
       )}
 
-      {lengthOfImages > 0 && !loading && (
+      {lengthOfImages > 0 && lengthOfImages === 12 && !loading && (
         <Button onClick={loadMoreBtn} loading={loading} />
       )}
 
@@ -76,104 +76,3 @@ export const App = () => {
     </AppWpapper>
   );
 };
-// export class App extends Component {
-//   state = {
-//     images: [],
-//     value: "",
-//     page: 1,
-//     error: "",
-//     loading: false,
-//     showModal: false,
-//     imageForModal: "",
-//   };
-
-// async componentDidUpdate(_, prevState) {
-//   const { value, page } = this.state;
-
-// try {
-//   if (prevState.value !== value) {
-//     const newImages = await getImages(value, page);
-//     newImages.length === 0 && toast.error('No such picture');
-//     this.setState({
-//       loading: true,
-//     });
-//     this.setState({
-//       loading: false,
-//       images: newImages,
-//     });
-//   }
-
-// } catch (error) {
-//   this.setState({
-//     error,
-//   });
-// }
-//   try {
-//     if (prevState.page !== page && page !== 1) {
-//       const newImages = await getImages(value, page);
-//       setTimeout(() => {
-//         this.setState({
-//           loading: false,
-//           images: [...prevState.images, ...newImages],
-//         });
-//       }, 1000);
-//     }
-//   } catch (error) {
-//     this.setState({
-//       status: "rejected",
-//     });
-//   }
-// }
-
-// toggleModal = (image) => {
-//   this.setState(({ showModal }) => ({
-//     showModal: !showModal,
-//     imageForModal: image,
-//   }));
-// };
-
-// loadMoreBtn = async () => {
-//   this.setState((prevState) => {
-//     return {
-//       page: prevState.page + 1,
-//       loading: true,
-//     };
-//   });
-// };
-
-// handleFormSubmit = async (image) => {
-//   this.setState({ page: 1, value: image });
-// };
-
-//   render() {
-//     const { images, loading, showModal,imageForModal } = this.state;
-//     const lengthOfImages = images.length;
-
-// return (
-//   <AppWpapper>
-//      {showModal && (
-//   <Modal onClose={this.toggleModal}>
-//     <img src={imageForModal} alt={'beautiful pic'} />
-//   </Modal>)}
-//     <Searchbar onSubmit={this.handleFormSubmit} />
-//     <Toaster/>
-//     {lengthOfImages === 0 && !loading && (
-//       <Text>Enter image name you are looking for.</Text>
-//     )}
-//     {lengthOfImages > 0 && (
-//       <ImageGallery
-//         images={images}
-//         onClick={this.toggleModal}
-//         showModal={showModal}
-//       />
-//     )}
-
-//     {lengthOfImages > 0 && !loading && (
-//       <Button onClick={this.loadMoreBtn} loading={loading} />
-//     )}
-
-//     {loading && <Loader />}
-//   </AppWpapper>
-// );
-//   }
-// }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -12,17 +12,14 @@ export const Modal = ({ onClose, children }) => {
         onClose();
       }
     };
+
     window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      console.log("delete func");
+      window.removeEventListener("keydown", handleKeyDown);
+    };
   }, [onClose]);
-  // useEffect(() => {
-  //   console.log("cyzkb");
-  //   const handleKeyDown = (e) => {
-  //     if (e.code === "Escape") {
-  //       onClose();
-  //     }
-  //   };
-  //   window.removeEventListener("keydown", handleKeyDown);
-  // }, [onClose]);
 
   const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
@@ -36,34 +33,6 @@ export const Modal = ({ onClose, children }) => {
     modalRoot
   );
 };
-// export class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-// componentWillUnmount() {
-//   window.removeEventListener('keydown', this.handleKeyDown);
-// }
-// handleKeyDown = (e) => {
-//   if (e.code === 'Escape') {
-//     this.props.onClose();
-//   }
-// };
-
-// handleBackdropClick = (e) => {
-//   if (e.currentTarget === e.target) {
-//     this.props.onClose();
-//   }
-// };
-
-//   render() {
-// return createPortal(
-//   <Overlay onClick={this.handleBackdropClick}>
-//     <ModalWindow>{this.props.children}</ModalWindow>
-//   </Overlay>,
-//   modalRoot
-// );
-//   }
-// }
 
 Modal.propTypes = {
   onClose: PropTypes.func.isRequired,
